ff78368efedf04bdf53e881c9ec6ece29953cc99
abdollah <danial.abdollahi@st.ovgu.de>
2024-10-17 22:09:47 +0200

    fished Spl initializations, mint, metadata

--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -1,5 +1,5 @@
 import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
+import wallet from "../Turbin3-wallet.json"
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
@@ -10,18 +10,39 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("Dsifggc2yzeczzVMsLDB5jj4XZ1y3ynkJGpRjwicnhoV");
 
 // Recipient address
 const to = new PublicKey("<receiver address>");
+const token_decimals = 1_000_000n;
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
-
+        const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+            connection,
+            keypair,
+            mint,
+            keypair.publicKey,
+        );
         // Get the token account of the toWallet address, and if it does not exist, create it
-
+        const toTokenAccount = await getOrCreateAssociatedTokenAccount(
+            connection,
+            keypair,
+            mint,
+            keypair.publicKey,
+        );
         // Transfer the new token to the "toTokenAccount" we just created
+        const signature = await transfer(
+            connection,
+            keypair,
+            fromTokenAccount.address,
+            toTokenAccount.address,
+            keypair,
+            1n * token_decimals,
+        )
+        console.log('Transfer success! Check out your TK here: \n\nhttps://explore.solana.com/tx/${singnature}?cluster=devnet');
+        
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
